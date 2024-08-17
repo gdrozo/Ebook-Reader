224aa1f434cb138c9ca2c1e3af2e93d7ea82712d
gdrozo <gd.rozo@uniandes.edu.co>
2024-08-17 03:23:42 -0500

    adsf

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ try {
   if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
       navigator.serviceWorker
-        .register('/service-worker.js')
+        .register('./service-worker.js')
         .then(registration => {
           console.log('Service Worker registered with scope:', registration.scope)
         })
