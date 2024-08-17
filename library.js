16a9ae244e83b870c0fe59559ee6aae68a40831e
gdrozo <gd.rozo@uniandes.edu.co>
2024-08-17 03:39:38 -0500

    fix?

--- library.js
+++ library.js
@@ -1,3 +1,20 @@
+try {
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker
+        .register('./service-worker.js')
+        .then(registration => {
+          console.log('Service Worker registered with scope:', registration.scope)
+        })
+        .catch(error => {
+          console.log('Service Worker registration failed:', error)
+        })
+    })
+  }
+} catch (error) {
+  alert(error)
+}
+
 window.onload = function () {
   // Your code here
 
