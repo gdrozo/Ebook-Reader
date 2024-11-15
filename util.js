e878591ff99a26766332a2d0a0502e5f7a9d7136
gdrozo <gd.rozo@uniandes.edu.co>
2024-11-14 22:52:02 -0500

    tt

--- util.js
+++ util.js
@@ -6,11 +6,11 @@ function registerServiceWorker() {
           .register('/service-worker.js')
           .then(registration => {
             console.log('Service Worker registered with scope:', registration.scope)
-            alert('Service Worker registered with scope:', registration.scope)
+            alert('Service Worker registered with scope: ' + registration.scope)
           })
           .catch(error => {
             console.log('Service Worker registration failed:', error)
-            alert('Service Worker registration failed:', error)
+            alert('Service Worker registration failed: ' + error)
           })
       })
     }
