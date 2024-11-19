eb2ec7251ed392c4326230ed7be93ab6c41e84a5
gdrozo <gd.rozo@uniandes.edu.co>
2024-11-18 21:55:46 -0500

    v5

--- engine.js
+++ engine.js
@@ -132,7 +132,7 @@ async function loadBook(url) {
     width: '100%',
     height: adjustedHeight,
     spread: 'always',
-    stylesheet: './css/injected.css',
+    stylesheet: '/css/injected.css',
   })
 
   return { book, rendition }
